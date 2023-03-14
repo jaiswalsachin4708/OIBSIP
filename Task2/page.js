8c48c4c30efaee629bf74b03dd324c74ab84a878
jaiswalsachin4708 <sachinjaiswal4708@gmail.com>
2023-03-15 00:23:48 +0530

    Tribute Page

---
+++ Task2/page.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $("#target1").click(function(){
+        $("#start").fadeToggle();
+    });
+     $("#show").click(function(){
+    $("#show1").toggle();
+    });
+  
+});
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 4000);
+
+}  
+  
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlide");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
